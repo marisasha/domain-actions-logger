ddc34fb1d5fffa3764d225da8279dd7d6fbb8e6a
Александр Маринушкин <marisasha228@bk.ru>
2026-03-31 14:19:42 +0300

    add domain_router

--- src/router.py
+++ src/router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
 from src.auth.service import router as auth_router
-
+from src.domain.service import router as domain_router
 
 main_router = APIRouter()
 
 main_router.include_router(auth_router)
+main_router.include_router(domain_router)
