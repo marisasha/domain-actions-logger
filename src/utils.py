c2424885138962b50e19186dc9212bb1bd432c30
Александр Маринушкин <marisasha228@bk.ru>
2026-04-03 02:52:29 +0300

    create: permission enum

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,16 @@
 from pathlib import Path
 
+from enum import Enum
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 ENV_PATH = BASE_DIR / ".env"
+
+
+class PermissionEnum(str, Enum):
+    USER = "user"
+    MODERATOR = "moderator"
+    ADMIN = "admin"
+
+
+def n_print(data):
+    print("\n\n\n\n\n\n", data, "\n\n\n\n\n\n")
