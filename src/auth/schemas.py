a45e6d59d88e0be52a1bdfd592833b277f9c0b9d
Александр Маринушкин <marisasha228@bk.ru>
2026-04-06 19:09:48 +0300

    fix: transport User's shemas from auth/schemas.py to user/schemas.py

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -1,35 +1,14 @@
 from datetime import datetime
 from pydantic import BaseModel
 
+from src.utils import GenderEnum
+
 
 class TokenSchema(BaseModel):
     access: str
     refresh: str
 
 
-class UserSchema(BaseModel):
-    username: str
-    first_name: str
-    last_name: str
-    email: str
-    birth_date: datetime
-    phone: str
-
-
-class UserSchemaResponse(UserSchema):
-    id: int
-
-
-class UserSchemaRequest(UserSchema):
-    password: str
-
-
 class UserAuthorizationSchema(BaseModel):
     username: str
     password: str
-
-
-class UserProfileSchema(BaseModel):
-    id: int
-    first_name: str
-    last_name: str
---
+++ src/user/schemas.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel
+
+from src.utils import GenderEnum
+
+
+class UserSchema(BaseModel):
+    username: str
+    first_name: str
+    last_name: str
+    gender: GenderEnum
+    email: str
+    birth_date: datetime
+    phone: str
+
+
+class UserSchemaResponse(UserSchema):
+    id: int
+
+
+class UserChangeDataSchema(BaseModel):
+    username: Optional[str] = None
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    email: Optional[str] = None
+    birth_date: Optional[datetime] = None
+    phone: Optional[str] = None
+
+
+class UserSchemaRequest(UserSchema):
+    password: str
+
+
+class UserProfileSchema(BaseModel):
+    id: int
+    first_name: str
+    last_name: str
+
+
+class MessageSchemaResponse(BaseModel):
+    message: str
