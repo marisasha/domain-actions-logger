51d5520fd46aa5218b3cba290f27fdd59a7482da
Александр Маринушкин <marisasha228@bk.ru>
2026-04-06 19:28:06 +0300

    fix: transport user model from auth/models.py to user/models.py

---
+++ src/user/models.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+import time
+from typing import Optional
+
+from sqlalchemy import CheckConstraint, ForeignKey, UniqueConstraint
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from src.database import Base
+
+
+class UserModel(Base):
+    __tablename__ = "user"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str]
+    first_name: Mapped[str]
+    last_name: Mapped[str]
+    gender: Mapped[str] = mapped_column(
+        CheckConstraint("gender IN ('M', 'F')", name="check_gender_valid"),
+    )
+    email: Mapped[str] = mapped_column(unique=True)
+    birth_date: Mapped[datetime]
+    phone: Mapped[str] = mapped_column(unique=True)
+    is_admin: Mapped[bool]
