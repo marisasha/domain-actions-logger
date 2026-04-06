aa73e13c6918eab333d81d4849a8b56ffabc91c0
Александр Маринушкин <marisasha228@bk.ru>
2026-04-06 19:13:24 +0300

    fix: -added user's id instead of a nickname in the token -added user's role in token -transport api create user to user/service

--- src/auth/service.py
+++ src/auth/service.py
@@ -2,72 +2,13 @@ from fastapi import APIRouter, FastAPI, HTTPException, Request
 from fastapi import status
 from sqlalchemy import exists, func, select
 
-from src.auth.models import *
 from src.auth.schemas import *
 from src.auth.dependencies import SessionDep
 from src.auth.security import *
 
+from src.user.models import UserModel
+
 router = APIRouter(tags=["auth"])
-
-
-@router.post(
-    "/auth/register", summary="Сreate new user", status_code=status.HTTP_201_CREATED
-)
-async def register(user: UserSchemaRequest, session: SessionDep) -> UserSchemaResponse:
-
-    try:
-
-        is_username_exists_execute = await session.execute(
-            select(exists().where(UserModel.username == user.username))
-        )
-
-        is_email_exists_execute = await session.execute(
-            select(exists().where(UserModel.email == user.email))
-        )
-
-        is_phone_exists_execute = await session.execute(
-            select(exists().where(UserModel.phone == user.phone))
-        )
-
-        username_exists = is_username_exists_execute.scalar()
-        email_exists = is_email_exists_execute.scalar()
-        phone_exists = is_phone_exists_execute.scalar()
-
-        if username_exists or email_exists or phone_exists:
-            detail = (
-                "Username already exists"
-                if username_exists
-                else (
-                    "Email already exists"
-                    if email_exists
-                    else "Phone already exists" if phone_exists else "Unknown error"
-                )
-            )
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail=detail,
-            )
-
-        new_user = UserModel(
-            username=user.username,
-            password=hash_password(user.password),
-            first_name=user.first_name,
-            last_name=user.last_name,
-            email=user.email,
-            birth_date=user.birth_date,
-            phone=user.phone,
-        )
-
-        session.add(new_user)
-        await session.commit()
-        return new_user
-    except HTTPException:
-        raise
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="Internal Server Error",
-        )
 
 
 @router.post(
@@ -83,7 +24,6 @@ async def login(data: UserAuthorizationSchema, session: SessionDep) -> TokenSche
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="This username does not exists",
         )
-
     is_user_verificate = verify_password(data.password, user.password)
 
     if not is_user_verificate:
@@ -91,9 +31,10 @@ async def login(data: UserAuthorizationSchema, session: SessionDep) -> TokenSche
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
         )
+    is_admin = "admin" if user.is_admin else "user"
 
-    access_token = create_access_token({"sub": data.username})
-    refresh_token = create_refresh_token({"sub": data.username})
+    access_token = create_access_token({"sub": str(user.id), "role": is_admin})
+    refresh_token = create_refresh_token({"sub": str(user.id)})
     return TokenSchema(access=access_token, refresh=refresh_token)
 
 
