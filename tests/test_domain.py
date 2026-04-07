b3019fc725d6b4fde9ee1c49e3f1ff5157f82a7f
Александр Маринушкин <marisasha228@bk.ru>
2026-04-08 01:54:03 +0300

    fix: delete owner domain tests

--- tests/test_domain.py
+++ tests/test_domain.py
@@ -33,139 +33,17 @@ async def get_access_token(client):
 
 
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-# =-=-=-=- Тест создания владельца домена =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-@pytest.mark.asyncio
-async def test_owner_success(client):
-
-    access_token = await get_access_token(client)
-    num = random.randint(0, 1000000000)
-
-    owner_json = owner_json = {
-        "first_name": "Тест",
-        "last_name": "Тест",
-        "gender": "M",
-        "email": f"test{num}@test.test",
-        "phone": f"{num}",
-        "birth_date": "2000-02-02T00:00:00",
-        "birth_place": "Москва",
-        "passport_from": "Россия",
-        "passport_number": str(num)[:6],
-        "passport_series": str(num)[:4],
-        "issue_date": "2000-02-02T00:00:00",
-        "expiry_date": None,
-        "department_code": "123-123",
-        "issue_by": "ГУ МВД ПО МОСКВЕ",
-    }
-
-    response = await client.post(
-        "api/owners",
-        json=owner_json,
-        headers={"Authorization": f"Bearer {access_token}"},
-    )
-
-    assert response.status_code == status.HTTP_201_CREATED
-    data = response.json()
-    assert data["email"] == f"test{num}@test.test"
-    print(f"[{datetime.now()}]TEST: owner success. RESULT: passed ✅")
-
-    # Тест на создание объекта с существующей почтой
-    response = await client.post(
-        "api/owners",
-        json=owner_json,
-        headers={"Authorization": f"Bearer {access_token}"},
-    )
-
-    assert response.status_code == status.HTTP_400_BAD_REQUEST
-    data = response.json()
-    assert "Email already exists" in data["detail"]
-
-    print(f"[{datetime.now()}]TEST: create owner with exists email . RESULT: passed ✅")
-
-    # Тест на создание объекта с существующим номером телефона
-    owner_json["email"] = f"new_test{num}@test.test"
-
-    response = await client.post(
-        "api/owners",
-        json=owner_json,
-        headers={"Authorization": f"Bearer {access_token}"},
-    )
-
-    assert response.status_code == status.HTTP_400_BAD_REQUEST
-    data = response.json()
-    assert "Phone already exists" in data["detail"]
-
-    print(f"[{datetime.now()}]TEST: create owner with exists phone . RESULT: passed ✅")
-
-    # Тест на создание объекта с существующим номером паспорта
-    owner_json["phone"] = f"+{num}"
-
-    response = await client.post(
-        "api/owners",
-        json=owner_json,
-        headers={"Authorization": f"Bearer {access_token}"},
-    )
-
-    assert response.status_code == status.HTTP_400_BAD_REQUEST
-    data = response.json()
-    assert "Passport number already exists" in data["detail"]
-
-    print(
-        f"[{datetime.now()}]TEST: create owner with exists passport number . RESULT: passed ✅"
-    )
-
-
-# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-# =-=-=-=- Тест получения владельца по ID =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-@pytest.mark.asyncio
-async def test_owner_get(client):
-    access_token = await get_access_token(client)
-
-    owner_id = 5
-    response = await client.get(
-        f"api/owners/{owner_id}",
-        headers={"Authorization": f"Bearer {access_token}"},
-    )
-
-    assert response.status_code == status.HTTP_200_OK
-    data = response.json()
-
-    assert "id" in data
-    assert data["id"] == owner_id
-    assert "first_name" in data
-    assert "last_name" in data
-    print(f"[{datetime.now()}]TEST: get owner . RESULT: passed ✅")
-
-    # Тест получения владельца с несуществующим id
-    owner_id = 999
-    response = await client.get(
-        f"api/owners/{owner_id}",
-        headers={"Authorization": f"Bearer {access_token}"},
-    )
-
-    assert response.status_code == status.HTTP_404_NOT_FOUND
-    data = response.json()
-    assert f"Owner with id {owner_id} not found" in data["detail"]
-    print(
-        f"[{datetime.now()}]TEST: get owner with not exists owner_id. RESULT: passed ✅"
-    )
-
-
-# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 # =-=-=-=- Тест создания домена =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 @pytest.mark.asyncio
 async def test_domain_success(client):
     access_token = await get_access_token(client)
 
-    owner_id = 5
     num = random.randint(0, 10000)
 
     response = await client.post(
         "api/domains",
         json={
-            "owner_id": owner_id,
             "name": f"test{num}.com",
             "registration_date": "2026-02-02T00:00:00",
             "expiry_date": "2026-04-02T00:00:00",
@@ -184,7 +62,6 @@ async def test_domain_success(client):
     response = await client.post(
         "api/domains",
         json={
-            "owner_id": owner_id,
             "name": f"test{num}.com",
             "registration_date": "2026-02-02T00:00:00",
             "expiry_date": "2026-04-02T00:00:00",
