3b836dee91854ff8190173f7f94dc6b0f24ada25
Александр Маринушкин <marisasha228@bk.ru>
2026-05-25 20:51:44 +0300

    create: add CORS settings, redis settings

--- src/main.py
+++ src/main.py
@@ -1,5 +1,23 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
+
+from contextlib import asynccontextmanager
+
+from src.redis_cache.service import redis_service
 from src.router import main_router
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await redis_service.connect()
+    print("Connected to Redis")
+
+    yield
+
+    await redis_service.disconnect()
+    print("Disconnected from Redis")
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(main_router)
+app.add_middleware(CORSMiddleware, allow_origins=["http://localhost:3000"])
