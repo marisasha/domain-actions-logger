adbdaaec9b37e796216516305bfd568908642648
Александр Маринушкин <marisasha228@bk.ru>
2026-05-25 20:52:04 +0300

    create: add MoveEnum

--- src/utils.py
+++ src/utils.py
@@ -12,10 +12,26 @@ class PermissionEnum(str, Enum):
     ADMIN = "admin"
     OWNER = "owner"
 
+    def __str__(self):
+        return self.value
+
 
 class GenderEnum(str, Enum):
     Male = "M"
     Female = "F"
+
+    def __str__(self):
+        return self.value
+
+
+class MoveEnum(str, Enum):
+    CREATE = "create"
+    READ = "read"
+    UPDATE = "update"
+    DELETE = "delete"
+
+    def __str__(self):
+        return self.value
 
 
 def n_print(data):
