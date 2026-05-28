6a4bb87e38a7192ba3c223600dd562e97f7dec5a
Александр Маринушкин <marisasha228@bk.ru>
2026-05-28 04:20:46 +0300

    create: include rabbitmq router

--- src/router.py
+++ src/router.py
@@ -3,9 +3,11 @@ from fastapi import APIRouter
 from src.auth.service import router as auth_router
 from src.user.service import router as user_router
 from src.domain.service import router as domain_router
+from src.rabbitmq.service import rabbitmq_service as rabbitmq_router
 
 main_router = APIRouter()
 
 main_router.include_router(auth_router)
 main_router.include_router(user_router)
 main_router.include_router(domain_router)
+main_router.include_router(rabbitmq_router.router)
