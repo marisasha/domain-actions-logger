711632d734e587cb1a50fecdefda50f1f411d54b
Александр Маринушкин <marisasha228@bk.ru>
2026-05-29 20:55:28 +0300

    create: verification model

--- src/user/models.py
+++ src/user/models.py
@@ -19,7 +19,18 @@ class UserModel(Base):
     gender: Mapped[str] = mapped_column(
         CheckConstraint("gender IN ('M', 'F')", name="check_gender_valid"),
     )
-    email: Mapped[str | None] = mapped_column(unique=True, nullable=True, default=None)
+    email: Mapped[str | None] = mapped_column(unique=True)
     birth_date: Mapped[datetime]
     phone: Mapped[str] = mapped_column(unique=True)
     is_admin: Mapped[bool]
+    is_email_verificated: Mapped[bool] = mapped_column(default=False)
+
+
+class VerificationModel(Base):
+    __tablename__ = "verification"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id", ondelete="CASCADE"))
+    code: Mapped[str]
+    expires_at: Mapped[datetime]
+    is_used: Mapped[bool] = mapped_column(default=False)
