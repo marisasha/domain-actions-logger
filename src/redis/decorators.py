b6ca207ab4fd774ea79c326df2e46e6513bbd8ec
Александр Маринушкин <marisasha228@bk.ru>
2026-05-29 20:54:40 +0300

    create: cache decorator

--- src/redis/decorators.py
+++ src/redis/decorators.py
@@ -1,6 +1,6 @@
 import hashlib
 import json
-from src.redis_cache.service import redis_service
+from src.redis.service import redis_service
 from functools import wraps
 from typing import Any, Callable, Type, Optional
 
