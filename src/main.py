675f68b876209a2f814f6befb91857a6bd6eaa30
Александр Маринушкин <marisasha228@bk.ru>
2026-06-06 16:17:10 +0300

    fix: rename redis.service to redis.config

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 
 from contextlib import asynccontextmanager
 
-from src.redis.service import redis_service
+from src.redis.config import redis_service
 from src.router import main_router
 
 
